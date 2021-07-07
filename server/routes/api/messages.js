521af2802f857d898f183fc1d272445261c913dd
jaykim97 <colinjkim1997@gmail.com>
2021-07-07 18:22:32 -0400

    added additional verification to prevent non-participating members from sending messages to conversation

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -10,10 +10,20 @@ router.post("/", async (req, res, next) => {
     }
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
-
     // if we already know conversation id, we can save time and just add it to message and return
+    // SECURITY WARNING 
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
+      let deter = false
+      await Conversation.findConversation(senderId, recipientId)
+        .then(conversation =>{
+          if (conversation.getDataValue("id") === conversationId){
+            deter = true
+          }
+        })
+      if(!deter){
+        return res.sendStatus(401);
+      }
+      const message = await Message.create({ senderId, text, conversationId});
       return res.json({ message, sender });
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
